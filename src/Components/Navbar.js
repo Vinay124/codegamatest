da8fd8b8d8cac0a41af6bb7844c44e2382c0ce86
Vinay124 <39564276+Vinay124@users.noreply.github.com>
2022-07-20 01:44:48 +0530

    main

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -44,11 +44,63 @@ export default function Navbar() {
               </a>
             </div>
         </nav>
-        <div className='menuheader'>
-            
-        </div>
+        
       </div>
     </header>
+    <div className='menuheader'>
+      <div className='container'>
+      <div className='down-menu'>
+        <div>
+        <a href='/'>
+            <img className="zoneimg"  src='https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100' alt="/"/>
+            <span>Top Offers</span>
+            </a>
+          </div>
+        <div>
+        <a href='/'>
+            <img className="zoneimg"  src='https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100' alt="/"/>
+              <span>Grocery</span>
+              </a>
+        </div>
+        <div>
+        <a href='/'>
+            <img className="zoneimg"  src='https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100' alt="/"/>
+              <span>Fashion</span>
+              </a>
+        </div>
+        <div>
+        <a href='/'>
+            <img className="zoneimg"  src='https://rukminim1.flixcart.com/flap/128/128/image/c12afc017e6f24cb.png?q=100' alt="/"/>
+              <span>Electronics</span>
+              </a>
+        </div>
+        <div>
+          <a href='/'>
+            <img className="zoneimg"  src='https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100' alt="/"/>
+              <span>Home</span>
+              </a>
+        </div>
+        <div>
+          <a href='/'>
+            <img className="zoneimg"  src='https://rukminim1.flixcart.com/flap/128/128/image/ab7e2b022a4587dd.jpg?q=100' alt="/"/>
+              <span>Appliances</span>
+              </a>
+        </div>
+        <div>
+          <a href='/'>
+            <img className="zoneimg"  src='https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100' alt="/"/>
+              <span>Travel</span>
+              </a>
+        </div>
+        <div>
+          <a href='/'>
+            <img className="zoneimg"  src='https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100' alt="/"/>
+              <span>Beauty, Toys & More</span>
+              </a>
+        </div>
+          </div>
+      </div>
+    </div>
   </>
   )
 }
