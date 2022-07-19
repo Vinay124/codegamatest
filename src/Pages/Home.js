3ae9b2876048d45e84e8951cf7e3d3da0901d0a9
Vinay124 <39564276+Vinay124@users.noreply.github.com>
2022-07-19 19:45:20 +0530

    main

---
+++ src/Pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../Styles/app.css'
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
---
+++ src/Pages/Product.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../Styles/app.css"
+
+export default function Product() {
+  return (
+    <div>'Product'</div>
+  )
+}
