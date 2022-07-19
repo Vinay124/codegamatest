1e1d90ff98b2e1120077d4027ee699e97d9221e9
Vinay124 <39564276+Vinay124@users.noreply.github.com>
2022-07-19 22:19:50 +0530

    main

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import "../Styles/app.css"
+import { SearchOutlined , DownOutlined } from '@ant-design/icons';
 
 export default function Navbar() {
   return (
-    <h1>hellow this is navbar</h1>
+    <>
+    <header className='header'>
+      <div className='container'>
+        <nav className='navmain'>
+            <div className='logo'>
+              <div>
+                <h1>MyCart</h1>
+              </div>
+              <div>
+                <span>Explore More</span>
+              </div>
+            </div>
+            <div className='search'>
+              <form className='form-search'>
+                <div className='search-box'>
+                <input type="text" className='search-field' name="text" placeholder='Search Products, brand and more'/>
+                <button className='submit-btn' type='submit'> 
+                <SearchOutlined style={{ fontSize: 20, color: "black" }}/></button>
+                </div>
+              </form>
+            </div>
+            <div className='Login'>
+              <button className='btn-primary'> Login</button>
+            </div>
+            <div className='sub-nav'>
+              <ul>
+                <li>Become a Seller</li>
+                <li>More <DownOutlined style={{ fontSize: 10, color: "white" }}/></li>
+              </ul>
+            </div>
+            <div className='cart'>
+              <button>
+                Cart
+              </button>
+            </div>
+        </nav>
+      </div>
+    </header>
+  </>
   )
 }
